01140519d4407cf809bb2a52f9fba13fec5f902c
Graf86X <isaf68ak@gmail.com>
2026-02-02 19:48:22 +0200

    Clean up whitespace in Binary search.cpp
    Removed unnecessary blank lines in the code.

---
+++ Binary search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int binary_search(int* arr, int x, int l, int r){
+	if (l > r) return -1;
+
+	int mid = l + (r - l) / 2;
+	if (arr[mid] == x) return mid;
+	if (arr[mid] > x) return binary_search(arr, x, l, mid - 1);
+	if (arr[mid] < x) return binary_search(arr, x, mid + 1, r);
+
+
+}
+
+void main() {
+	int arr[] = { 1, 3, 5, 7, 9 };
+	int n = 5;
+
+	int idx = binary_search(arr, 5, 0, n - 1);
+
+	std::cout << idx << "\n";
+}
